f9018bedab7360b27d72fec8e21cdf968556bc6d
brakdag <brakdag@gmail.com>
2018-06-06 12:26:45 -0300

    cambios

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-function retorno(data){
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/datos', function (req, res) {
+  getEducacionales(retorno,res);
+});
+
+app.listen(server_port, function () {
+  console.log('Servidor corriendo.');
+});
+
+function retorno(data,res){
   const tabletojson = require('tabletojson');
   const cheerio = require('cheerio');
   var $ = cheerio.load(data);
   var text = "<table>"+$('#example').html() + "</table>";
   var tablesAsJson = tabletojson.convert(text)[0];
   llamados=tablesAsJson.filter(a=>a[2]=="SAN RAFAEL").filter(a=>a[1]=="Secundario Técnico")
-  //console.log("cantidad: " + llamados.length)
-  //llamados.forEach(element => {console.log(element[8]+ "|" +element[3] +" | "+ element[13] )});
-  superPosicionHoraria(llamados[1][13])
+  console.log("cantidad de llamados: " + llamados.length)
+  visible = []
+  llamados.forEach(element => {visible.push("<td>"+element[8]+ "</td><td>" +element[3] + "</td><td>" + element[13] + "</td>" )});
+  console.log(llamados[1])
+  var page = "<html><body><table>" 
+  visible.forEach(a=>page+="<tr>"+a+"</tr>")
+  page+="</table></body></html>"
+  res.send(page);
 }
 
-horario = {dia:"",hora_inicio:"",hora_fin :""}
-function superPosicionHoraria(horarios,mis_horarios)
-{
-  
-  console.log(horarios)
-}
 
-
-function getEducacionales(retorno){
+function getEducacionales(retorno,res){
   const https = require('http');
   https.get('http://www.mendoza.edu.ar/educacionales/', (resp) => {
   var data = "";
@@ -27,7 +42,7 @@ function getEducacionales(retorno){
     data += chunk;
   });
   resp.on('end', () => {
-   retorno(data);
+   retorno(data,res);
   });
 
 }).on("error", (err) => {
@@ -35,9 +50,3 @@ function getEducacionales(retorno){
 });
 };
 
-function track(){
-getEducacionales(retorno);
-}
-//*[@id="example"]
-track();
-//setInterval(track,10000);
